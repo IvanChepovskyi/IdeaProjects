9b417367ef0d68dd8f828fb2cc1de99b4dc83c2c
IvanChepovskyi <agentvanya@gmail.com>
2017-09-28 10:42:05 +0300

    Add print() method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        args = new String[3];
+        for (int i = 0; i < args.length; i++){
+            args[i] = "" + i + i + i;
+        }
+	    print(args);
+    }
+    public static void print(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
